bbfc9729c021b16c5d5e28fec522e0d396d54f8d
Rolf Hut <r.w.hut@tudelft.nl>
2016-01-21 18:03:15 +0100

    adapted, no does work.

--- code/readDigitalTime/readDigitalTime.ino
+++ code/readDigitalTime/readDigitalTime.ino
@@ -1,64 +1,72 @@
-
-
+/* 
+script do to time measurements (number of pulses within an interval. 
+- every second, a measurement (as integer) is written over the Serial line
+- every 5 seconds, a measurement (as integer) is published to the Cloud
+- the function "readPulses" is call-able from the Cloud. A measurement (as integer!)
+  is returned. 
+*/
 
 //the interval used to measure, in milliseconds
 int measureTime = 1000;
 
+//the interval used to measure, in milliseconds
+int measureTimeOnline = 5000;
+//the name of the event.
+String eventName = "measurement/JuniorTUDelft/studentName";
+
 //the pin to use for measuring.
-String analogPin = "A0";
+String digitalPin = "D0";
 
 //some constants and constructs
-int measuredBitValue = 0;
-float measuredValue = 0.0;
+int measuredPulsesSerial = 0;
+int measuredPulsesOnline = 0;
+int measuredPulsesSerialPrevious = 0;
 
-Timer measureTimer(measureTime, readAndSend);
+Timer measureTimer(measureTime, readAndSerial);
+Timer measureTimerOnline(measureTimeOnline, readAndPublish);
 
 void setup(){
   //start Serial communication
   Serial.begin(9600);
 
   //set analogPin to INPUT
-  int pin = analogPin.toInt();
+  int pin = digitalPin.toInt();
   pinMode(pin, INPUT);
+
+  //attach an interupt for handling counting the pulses
+  attachInterrupt(pin, pulse, RISING);
 
   //start timer that calls measurement en sends results over Serial
   measureTimer.start();
+  measureTimerOnline.start();
+  
 
-  //make measuredValue available online
-  Particle.variable("measurement",measuredValue);
+  //make measuredPulsesSerialPrevious available online
+  Particle.variable("measurement",measuredPulsesSerialPrevious);
 
   //make readAnalogValue available online, returns measurement as integer
-  Particle.function("readAnalog",readAnalogValue);
+  Particle.function("readPulsesPerSecond",readPulses);
 }
 
 void loop(){}
 
-void readAndSend(){
-  readAnalogValue(analogPin);
-  Serial.println(measuredValue);
+void readAndSerial(){
+  Serial.println(measuredPulsesSerial);
+  measuredPulsesSerialPrevious=measuredPulsesSerial;
+  measuredPulsesSerial=0;
 }
 
-//readAnalog is done in two steps to allow calling readAnalogValue from the Cloud
-int readAnalogValue(String Command){
-  int pin = Command.toInt();
-  if (getPinMode(pin)==INPUT){
-    measuredBitValue = analogRead(pin);
-    measuredValue = convertBit2Measured(measuredBitValue);
-    return measuredBitValue;
-  } else {
-    measuredBitValue = -1;
-    measuredValue = -1.0;
-    return -1;
-  }
+void readAndPublish(){
+  Particle.publish(eventName, (String) measuredPulsesOnline);
+  measuredPulsesOnline=0;
 }
 
-float convertBit2Measured (int bitValue){
-  //first convert to Voltage
-  float voltageValue = (float) bitValue * 3.3 / 4096.0;
+//readPulses only returns the nr of pulses from the previous second.
+int readPulses(String Command){
+  return measuredPulsesSerialPrevious;
+}
 
-  //then convert to measured value using calibration. Change the line below as
-  //needed.
-  float value = voltageValue;
-
-  return value;
+void pulse(){
+    measuredPulsesSerial++;
+    measuredPulsesOnline++;
 }
