572038151550c4c805a7e41e7d17db4136d5fc04
Rolf Hut <r.w.hut@tudelft.nl>
2016-01-21 17:39:59 +0100

    added publish to cloud with timer

--- code/readAnalogSimple/readAnalogSimple.ino
+++ code/readAnalogSimple/readAnalogSimple.ino
@@ -1,31 +1,80 @@
+/* 
+script do to analog measurements. 
+- every second, a measurement (as floating point) is written over the Serial line
+- every 5 seconds, a measurement (as floating point) is published to the Cloud
+- the function "readAnalog" is call-able from the Cloud. A measurement (as integer!)
+  is returned. 
+*/
 
-//the pin used to measure
-int analogPin = A0;
+//the interval used to measure, in milliseconds
+int measureTime = 1000;
 
-//the measurement
-int measuredValue = 0;
+//the interval used to measure, in milliseconds
+int measureTimeOnline = 5000;
+//the name of the event.
+String eventName = "measurement/JuniorTUDelft/studentName";
+
+//the pin to use for measuring.
+String analogPin = "A0";
+
+//some constants and constructs
+int measuredBitValue = 0;
+float measuredValue = 0.0;
+
+Timer measureTimer(measureTime, readAndSerial);
+Timer measureTimerOnline(measureTimeOnline, readAndPublish);
 
 void setup(){
-  //tell the photon that analogPin is used as an INPUT
-  pinMode(analogPin, INPUT);
-
-  //begin Serial communication
+  //start Serial communication
   Serial.begin(9600);
 
-  //make the measurement available in the Cloud
+  //set analogPin to INPUT
+  int pin = analogPin.toInt();
+  pinMode(pin, INPUT);
+
+  //start timer that calls measurement en sends results over Serial
+  measureTimer.start();
+
+  //make measuredValue available online
   Particle.variable("measurement",measuredValue);
 
+  //make readAnalogValue available online, returns measurement as integer
+  Particle.function("readAnalog",readAnalogValue);
 }
 
-void loop(){
+void loop(){}
 
-  //do a measurement
-  measuredValue = analogRead(analogPin);
-
-  //send over Serial
+void readAndSerial(){
+  readAnalogValue(analogPin);
   Serial.println(measuredValue);
+}
 
-  //wait for a second
-  delay(1000);
+void readAndPublish(){
+  readAnalogValue(analogPin);
+  Particle.publish(eventName, (String) measuredValue);
+}
 
+//readAnalog is done in two steps to allow calling readAnalogValue from the Cloud
+int readAnalogValue(String Command){
+  int pin = Command.toInt();
+  if (getPinMode(pin)==INPUT){
+    measuredBitValue = analogRead(pin);
+    measuredValue = convertBit2Measured(measuredBitValue);
+    return measuredBitValue;
+  } else {
+    measuredBitValue = -1;
+    measuredValue = -1.0;
+    return -1;
+  }
+}
+
+float convertBit2Measured (int bitValue){
+  //first convert to Voltage
+  float voltageValue = (float) bitValue * 3.3 / 4096.0;
+
+  //then convert to measured value using calibration. Change the line below as
+  //needed.
+  float value = voltageValue;
+
+  return value;
 }
